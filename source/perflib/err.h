217e723db971ad82c23b569c04c327de5ae7d203
Alexandr Nedvedicky <sashan@openssl.org>
2025-07-15 18:01:55 +0200

    add quic_server_ssl_poll_http to benchmark SSL_poll() for QUIC protocol
    The tool spawns server and client thread. Client then starts '-c' connections
    to server over a loopback interface. The connections then transport
    bidirectional (-b) or unidirectional (-u) HTTP/1.0 streams to measure
    performance.
    
    Fixes #26
    
    Reviewed-by: Matt Caswell <matt@openssl.org>
    Reviewed-by: Neil Horman <nhorman@openssl.org>
    (Merged from https://github.com/openssl/perftools/pull/26)

---
+++ source/perflib/err.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2025 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef OSSL_PERFLIB_GETOPT_H
+# define OSSL_PERFLIB_GETOPT_H
+# pragma once
+
+#include <stdarg.h>
+
+extern const char *progname;
+
+extern void vwarnx(const char *, va_list);
+extern void errx(int, const char *, ...);
+extern void warnx(const char *, ...);
+
+#endif
+
