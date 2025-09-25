393f457ac16cb34cef92cc987071c52e14c015c7
Eugene Syromiatnikov <esyr@openssl.org>
2025-09-26 01:26:52 +0200

    perflib/err.c: use program_invocation_name on glibc
    Signed-off-by: Eugene Syromiatnikov <esyr@openssl.org>

--- source/perflib/err.c
+++ source/perflib/err.c
@@ -11,13 +11,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <openssl/e_os2.h>
+
 const char *progname;
+
+static ossl_inline void *
+get_progname(void)
+{
+    if (progname != NULL)
+        return progname;
+
+#if defined(__GLIBC__)
+    if (program_invocation_name && program_invocation_name[0])
+        return program_invocation_name;
+#endif
+
+    return NULL;
+}
 
 void
 vwarnx(const char *fmt, va_list ap)
 {
-    if (progname != NULL)
-        fprintf(stderr, "%s: ", progname);
+    if (get_progname() != NULL)
+        fprintf(stderr, "%s: ", get_progname());
     vfprintf(stderr, fmt, ap);
     putc('\n', stderr);
 }
